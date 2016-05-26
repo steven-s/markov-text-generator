96b24ecb596afdf4e051070337a937c60641bc7e
Steven Samson <kosai03@gmail.com>
2016-05-26 00:39:46 -0700

    Updated generate script options

--- README.md
+++ README.md
@@ -1,2 +1,16 @@
 # Markov Text Generator
 This tool generates markov text of specified lengths based on an input document
+
+## MarkovGenerator class
+
+This class does the actual heavy listing of generating the Markov chain from an input document.
+
+The constructor provides an optional arg, `ngram_size` (default of 2) to choose the size of the n-grams that are generated from the source text
+
+`ingest_text` takes a string that represents the source text to generate a chain from
+
+`generate_string` will spit out a string with `length` (optional, default 14) words. It also provides an optional `use_weighted_transition` arg to weight the next random n-gram choice while building the string
+
+## generate.py
+
+`generate.py` is a utility script to easily build random markov text from the command line
--- generate.py
+++ generate.py
@@ -6,26 +6,30 @@ from  markov_generator import MarkovGenerator
 parser = argparse.ArgumentParser()
 parser.add_argument("-v", "--verbose", help="increase output verbosity",
                     action="store_true")
-parser.add_argument("source_text_location", help="source text file")
-parser.add_argument("output_length", help="word length of sentence to generate", type=int)
+parser.add_argument("--ngram-size", help="set the size of n-grams", type=int, default=2)
+parser.add_argument("--use-weighted-transitions", help="use weighted transitions", action="store_true", default=False)
+parser.add_argument("source_text_location", metavar="SOURCE_TEXT_LOCATION", help="source text file")
+parser.add_argument("output_length", metavar="OUTPUT_LENGTH", help="word length of sentence to generate", type=int)
 
 args = parser.parse_args()
 
 if args.verbose: print("starting markov generator")
 
 if args.verbose:
+    print("ngram size: {}".format(args.ngram_size))
+    print("use weighted transitions: {}".format(args.use_weighted_transitions))
     print("source file: {}".format(args.source_text_location))
     print("output length: {}".format(args.output_length))
 
 if args.verbose: print("building model")
 
-generator = MarkovGenerator(ngram_size=3)
+generator = MarkovGenerator(ngram_size=args.ngram_size)
 
 with open(args.source_text_location, 'r') as source_file:
     source_text = source_file.read()
     generator.ingest_text(source_text)
 
 if args.verbose: print ("generating string")
-chain_sentence = generator.generate_string(args.output_length, True)
+chain_sentence = generator.generate_string(args.output_length, args.use_weighted_transitions)
 
 print(chain_sentence)
